ab566b06824917a20616ff02bfaae9efa8fff363
Darren Weston <darren.weston@kcl.ac.uk>
2026-02-21 13:14:59 +0000

    Add react-router

--- ts/src/main.tsx
+++ ts/src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Home from './pages/Home.tsx'
+import About from './pages/About.tsx'
+import NotFound from './pages/NotFound.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'about', element: <About /> },
+      { path: '*', element: <NotFound /> },
+    ],
+  },
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
