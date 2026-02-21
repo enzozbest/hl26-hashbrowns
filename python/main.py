5dec1231d5524b9a6c92c9aa6707c00a9318ce42
Enzo Zuanazi Bestetti <enzobestetti3@gmail.com>
2026-02-21 17:04:20 +0000

    Add main.py

--- python/main.py
+++ python/main.py
@@ -5,7 +5,7 @@ from hashbrowns.ibex.client import IbexClient
 
 async def main():
     async with IbexClient(settings) as client:
-        results = await client.search([528349, 186246], radius=300, srid=27700)
+        results = await client.search([528349, 186246], 50, 27700)
         print(results)
 
 if __name__ == "__main__":
