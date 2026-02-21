2ba7806e2e040e5a4885722f8df77c370200c4c2
aquadoot <ikinjenny+programming@gmail.com>
2026-02-21 17:18:19 +0000

    Add landing page text and graphics

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,15 +1,5 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
-      <nav className="flex gap-4 p-4 bg-white shadow dark:bg-gray-800">
-        <Link to="/" className="hover:text-blue-500">Home</Link>
-        <Link to="/about" className="hover:text-blue-500">About</Link>
-      </nav>
-      <main className="max-w-3xl mx-auto p-6">
-        <Outlet />
-      </main>
-    </div>
-  )
+  return <Outlet />
 }
