e97db964d6d0d828ded4a1ffc547e7ebb173124d
Darren Weston <darren.weston@kcl.ac.uk>
2026-02-22 06:40:58 +0000

    CORS

--- python/main.py
+++ python/main.py
@@ -1,9 +1,19 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import analyse, councils
 
+
 app = FastAPI(title="Hashbrowns Planning Intelligence API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(councils.router)
 app.include_router(analyse.router)
